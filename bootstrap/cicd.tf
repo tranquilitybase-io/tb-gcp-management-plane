f22585de9b64502d292435bd32b1ea44b53304bc
e-amce <adam.crow@gft.com>
2021-03-02 16:08:05 +0000

    Created cloudbuild.yaml Create cloudbuild trigger

--- bootstrap/cicd.tf
+++ bootstrap/cicd.tf
@@ -2,3 +2,20 @@ resource "google_sourcerepo_repository" "tb-management-plane-repo" {
   name    = local.source_repo_name
   project = var.project_id
 }
+
+resource "google_cloudbuild_trigger" "bootstrap-cb" {
+
+  trigger_template {
+    tag_name   = "0.1.0"
+    repo_name = google_sourcerepo_repository.tb-management-plane-repo.name
+    project_id = var.project_id
+  }
+
+  substitutions = {
+    _PROJECT_ID = var.project_id
+    _REGION     = var.region
+  }
+
+  project = var.project_id
+  filename = "cloudbuild.yaml"
+}
